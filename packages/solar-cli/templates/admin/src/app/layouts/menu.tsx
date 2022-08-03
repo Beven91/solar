113db234076b1ac01a0af71bf929a90399fbaa52
beven.zhou@hotmail.com <beven.zhou@hotmail.com>
2022-08-03 21:44:05 +0800

    调整icon

--- packages/solar-cli/templates/admin/src/app/layouts/menu.tsx
+++ packages/solar-cli/templates/admin/src/app/layouts/menu.tsx
@@ -1,12 +1,18 @@
+import React from 'react';
+import { UserOutlined, SettingOutlined } from '@ant-design/icons';
+import { AbstractMenuType } from 'solar-pc/src/interface';
+
 export default [
   {
+    icon: <UserOutlined />,
     name: '引导页',
     key: 'home',
     href: '#/',
   },
   {
+    icon: <SettingOutlined />,
     name: '列表页',
     key: 'example',
     href: '#/example/list',
   },
-];
+] as AbstractMenuType[];
